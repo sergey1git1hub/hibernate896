61eb1b96d0ba8d5559fa6f9511880c315372012e
schubuk <nameserhiy@gmail.com>
2018-01-19 17:17:01 +0200

    End of lesson 19. Table per class.

--- src/org/sergey/hibernate/HibernateTest.java
+++ src/org/sergey/hibernate/HibernateTest.java
@@ -1,6 +1,8 @@
 package org.sergey.hibernate;
 
 import org.sergey.javabrains.dto.Address;
+import org.sergey.javabrains.dto.FourWheeler;
+import org.sergey.javabrains.dto.TwoWheeler;
 import org.sergey.javabrains.dto.UserDetails;
 import org.sergey.javabrains.dto.Vehicle;
 
@@ -11,37 +13,59 @@ import org.hibernate.SessionFactory;
 public class HibernateTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		UserDetails user = new UserDetails();
-		//user.setUserId(1);
-		user.setUserName("Sergey");
-		
-		Vehicle  vehicle = new Vehicle();
+		Vehicle vehicle =new Vehicle();
 		vehicle.setVehicleName("car");
 		
-		Vehicle vehicle2 = new Vehicle();
-		vehicle2.setVehicleName("jeep");
+		TwoWheeler bike = new TwoWheeler();
+		bike.setVehicleName("Bike");
+		bike.setSteeringHandle("Bike Steering Handle.");
 		
-		user.getVehicle().add(vehicle);
-		user.getVehicle().add(vehicle2);
+		FourWheeler car = new FourWheeler();
+		car.setVehicleName("Porshe");
+		car.setSteeringWheel("Steering Wheel.");
 		
-		vehicle.getUserList().add(user);
-		vehicle2.getUserList().add(user);
-	
+		
+		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
+		
 		session.beginTransaction();
-		session.save(user);
 		session.save(vehicle);
-		session.save(vehicle2);
+		session.save(bike);
+		session.save(car);
+		
 		session.getTransaction().commit();
 		session.close();
 		
-		UserDetails userFromDB = null;
-		session = sessionFactory.openSession();
-		session.beginTransaction();
-		userFromDB = (UserDetails) session.get(UserDetails.class, 1);
-		System.out.println("User Name retrieved: " + userFromDB.getUserName());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 
 	}
--- src/org/sergey/javabrains/dto/Vehicle.java
+++ src/org/sergey/javabrains/dto/Vehicle.java
@@ -1,16 +1,23 @@
 package org.sergey.javabrains.dto;
 
 import java.util.ArrayList;
+
 import java.util.Collection;
 
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.InheritanceType;
 
 @Entity
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
+//@DiscriminatorColumn(name = "VEHICLE_TYPE", discriminatorType = DiscriminatorType.STRING)
 public class Vehicle {
 	@Id @GeneratedValue
 	private int vehicleId;
